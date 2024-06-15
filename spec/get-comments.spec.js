ac4d9f3ea3dee435e38893d1149c9f56efa66d98
lmnleaf <lmnleaf@users.noreply.github.com>
2024-06-15 21:13:41 +0000

    updates moctokit to accept mockData as an argument

--- spec/get-comments.spec.js
+++ spec/get-comments.spec.js
@@ -2,15 +2,41 @@ const getComments = require('../src/get-comments.js');
 const Moctokit = require('./support/moctokit.js');
 
 describe("Get Comments", function() {
-  let octokit = new Moctokit();
+  let octokit;
   let owner = 'org';
   let repo = 'repo';
+  let mockData = [
+    { 
+      user: { login: 'github-advanced-security[bot]' }, 
+      pull_request_url: 'https://api.github.com/repos/org/repo/pulls/10', 
+      created_at: new Date().toISOString() 
+    }, // Comment from today
+    { 
+      user: { login: 'commenter1' }, 
+      pull_request_url: 'https://api.github.com/repos/org/repo/pulls/9', 
+      created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 15)).toISOString() 
+    }, // Comment from 15 days ago
+    { 
+      user: { login: 'commenter2' }, 
+      pull_request_url: 'https://api.github.com/repos/org/repo/pulls/9', 
+      created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 17)).toISOString() 
+    }, // Comment from 17 days ago
+    { 
+      user: { login: 'commenter2' }, 
+      pull_request_url: 'https://api.github.com/repos/org/repo/pulls/7', 
+      created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 31)).toISOString() 
+    } // Comment from 31 days ago
+  ]
+
+  beforeEach(() => {
+    octokit = new Moctokit(mockData);
+  });
 
   it('gets PR comments for the past 30 days', async function () {
     const comments = await getComments(owner, repo, octokit);
 
     expect(octokit.paginate).toHaveBeenCalled();
-    expect(comments.length).toBe(3); // Expecting 2 comments that are within the last 30 days
+    expect(comments.length).toBe(3); // E xpecting 3 comments that are within the last 30 days
     expect(new Date(comments[0].created_at) <= new Date()).toBeTrue();
     expect(new Date(comments[1].created_at) <= new Date()).toBeTrue();
     expect(new Date(comments[2].created_at) <= new Date()).toBeTrue();
--- spec/support/moctokit.js
+++ spec/support/moctokit.js
@@ -1,49 +1,23 @@
 class Moctokit {
-    constructor() {
+    constructor(mockData) {
       this.rest = {
         issues: {
           listCommentsForRepo: this.listCommentsForRepo
+        },
+        pulls: {
+          list: this.listPRs
         }
       };
       this.paginate = this.mockPaginate;
-    }
-  
-    listCommentsForRepo() {
-      return Promise.resolve({
-        data: [
-          { user: { login: 'github-advanced-security[bot]' }, pull_request_url: 'https://api.github.com/repos/org/repo/pulls/9' },
-          { user: { login: 'commenter1' }, pull_request_url: 'https://api.github.com/repos/org/repo/pulls/10' }
-        ]
-      });
+      this.mockData = mockData;
     }
 
     mockPaginate = jasmine.createSpy('paginate').and.callFake((method, options, callback) => {
-      const comments = [
-        { 
-          user: { login: 'github-advanced-security[bot]' }, 
-          pull_request_url: 'https://api.github.com/repos/org/repo/pulls/10', 
-          created_at: new Date().toISOString() 
-        }, // Comment from today
-        { 
-          user: { login: 'commenter1' }, 
-          pull_request_url: 'https://api.github.com/repos/org/repo/pulls/9', 
-          created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 15)).toISOString() 
-        }, // Comment from 15 days ago
-        { 
-          user: { login: 'commenter2' }, 
-          pull_request_url: 'https://api.github.com/repos/org/repo/pulls/9', 
-          created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 17)).toISOString() 
-        }, // Comment from 17 days ago
-        { 
-          user: { login: 'commenter2' }, 
-          pull_request_url: 'https://api.github.com/repos/org/repo/pulls/7', 
-          created_at: new Date(Date.now() - (24 * 60 * 60 * 1000 * 31)).toISOString() 
-        } // Comment from 31 days ago
-      ];
-      const response = { data: comments };
+      const mockData = this.mockData;
+      const response = { data: mockData };
       const done = jasmine.createSpy('done');
       callback(response, done);
-      return Promise.resolve(comments.filter(comment => new Date(comment.created_at) > options.thirtyDaysAgo));
+      return Promise.resolve(mockData.filter(someData => new Date(someData.created_at) > options.thirtyDaysAgo));
     })
   }
   
