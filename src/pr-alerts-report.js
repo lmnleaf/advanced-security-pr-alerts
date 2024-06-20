04afa8a00d5d04cb114c0169426370785ddc67ac
lmnleaf <lmnleaf@users.noreply.github.com>
2024-06-20 15:29:04 -0600

    minor refactor

--- src/pr-alerts-report.js
+++ src/pr-alerts-report.js
@@ -19,66 +19,8 @@ async function createReport(owner, repos, octokit) {
 
       return info;
     });
-  
-    const csvRows = alerts.map((alert) => [
-      alert.number,
-      alert.rule.id,
-      alert.rule.security_severity_level,
-      alert.rule.severity,
-      alert.rule.description,
-      alert.state,
-      alert.most_recent_instance.state,
-      alert.most_recent_instance.ref,
-      alert.most_recent_instance.commit_sha,
-      alert.most_recent_instance.location.path,
-      alert.tool.name,
-      alert.tool.version,
-      alert.fixed_at,
-      alert.dismissed_at,
-      alert.dismissed_by,
-      alert.dismissed_reason,
-      alert.dismissed_comment,
-      alert.created_at,
-      alert.updated_at,
-      alert.pr.repo,
-      alert.pr.number,
-      alert.pr.user,
-      alert.pr.state,
-      alert.pr.draft,
-      alert.pr.merged_at,
-      alert.pr.updated_at
-    ]);
-  
-    csvRows.unshift([
-      'number',
-      'rule_id',
-      'rule_security_severity_level',
-      'rule_severity',
-      'description',
-      'state',
-      'most_recent_instance_state',
-      'most_recent_instance_ref',
-      'most_recent_commit_sha',
-      'most_recent_instance_path',
-      'tool',
-      'tool_version',
-      'fixed_at',
-      'dismissed_at',
-      'dismissed_by',
-      'dismissed_reason',
-      'dismissed_comment',
-      'created_at',
-      'updated_at',
-      'repo',
-      'pr_number',
-      'pr_user',
-      'pr_state',
-      'pr_draft',
-      'pr_merged_at',
-      'pr_updated_at'
-    ]);
 
-    writeReport(csvRows);
+    writeReport(alerts);
   } catch (error) {
     throw error;
   }
@@ -88,7 +30,65 @@ async function createReport(owner, repos, octokit) {
   return reportSummary(message, repos, alertInfo);
 }
 
-function writeReport (csvRows) {
+function writeReport (alerts) {
+  const csvRows = alerts.map((alert) => [
+    alert.number,
+    alert.rule.id,
+    alert.rule.security_severity_level,
+    alert.rule.severity,
+    alert.rule.description,
+    alert.state,
+    alert.most_recent_instance.state,
+    alert.most_recent_instance.ref,
+    alert.most_recent_instance.commit_sha,
+    alert.most_recent_instance.location.path,
+    alert.tool.name,
+    alert.tool.version,
+    alert.fixed_at,
+    alert.dismissed_at,
+    alert.dismissed_by,
+    alert.dismissed_reason,
+    alert.dismissed_comment,
+    alert.created_at,
+    alert.updated_at,
+    alert.pr.repo,
+    alert.pr.number,
+    alert.pr.user,
+    alert.pr.state,
+    alert.pr.draft,
+    alert.pr.merged_at,
+    alert.pr.updated_at
+  ]);
+
+  csvRows.unshift([
+    'number',
+    'rule_id',
+    'rule_security_severity_level',
+    'rule_severity',
+    'description',
+    'state',
+    'most_recent_instance_state',
+    'most_recent_instance_ref',
+    'most_recent_commit_sha',
+    'most_recent_instance_path',
+    'tool',
+    'tool_version',
+    'fixed_at',
+    'dismissed_at',
+    'dismissed_by',
+    'dismissed_reason',
+    'dismissed_comment',
+    'created_at',
+    'updated_at',
+    'repo',
+    'pr_number',
+    'pr_user',
+    'pr_state',
+    'pr_draft',
+    'pr_merged_at',
+    'pr_updated_at'
+  ]);
+
   let csvDate = new Date().toISOString().slice(0, 10);
 
   // TO DO: update path
